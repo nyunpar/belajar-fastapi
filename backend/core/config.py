c9bffc5b65d375154543249b9241ad1715acedc9
nirwan <ny.unpar@gmail.com>
2022-09-19 22:23:13 +0700

    connect to database

--- backend/core/config.py
+++ backend/core/config.py
@@ -9,11 +9,11 @@ class Settings:
     PROJECT_TITLE: str="Jobboard"
     PROJECT_VERSION: str="0.1.1"
     
-    POSTGRES_USER : str = os.getenv("POSTGES_USER")
-    POSTGRES_PASSWORD : str = os.getenv("POSTGES_PASSWORD")
-    POSTGRES_SERVER : str = os.getenv("POSTGES_SERVER","localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGES_PORT",5432)
-    POSTGRES_DB : str = os.getenv("POSTGES_DB","db_course")
+    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD : str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432)
+    POSTGRES_DB : str = os.getenv("POSTGRES_DB","db_course")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}" 
     
 settings = Settings() 
